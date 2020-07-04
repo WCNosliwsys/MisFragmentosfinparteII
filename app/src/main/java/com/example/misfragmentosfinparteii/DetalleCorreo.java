d98fb19b8d206ae5e6c47e0a082b911f297ad5de
Wilson Callisaya <demarktec@gmail.com>
2020-07-03 22:20:49 -0500

    Enviando parametros del Adaptador a la actividad detalle correo

--- app/src/main/java/com/example/misfragmentosfinparteii/DetalleCorreo.java
+++ app/src/main/java/com/example/misfragmentosfinparteii/DetalleCorreo.java
@@ -3,12 +3,19 @@ package com.example.misfragmentosfinparteii;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetalleCorreo extends AppCompatActivity {
 
+    TextView txtdetalle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_correo);
+        txtdetalle = findViewById(R.id.TxtDetalle);
+        txtdetalle.setText("De: "+ getIntent().getExtras().getString("de")+
+                "Asunto: "+ getIntent().getExtras().getString("asunto")+
+                "Contenido: "+ getIntent().getExtras().getString("texto"));
+
     }
 }
